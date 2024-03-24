6e4c16fc6b25e6d852bcaabb25e7887c591d7445
S. M. SADI <smsadi2001@gmail.com>
2024-03-24 19:02:22 +0600

    Add files via upload

---
+++ reglog/view/profile info.php
@@ -0,0 +1,65 @@
+<?php
+    include("dashboardHeader.html")
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>Hey,  Update Information</h1>
+
+    <label for="FullName">Full Name : </label>
+    <input type="text" name="FullName" id="FullName" required value="">
+
+    <br>
+
+    <label for="address">Address : </label>
+    <input type="text" name="address" id="address" required value="">
+
+    <label for="FullName">Full Name : </label>
+    <input type="text" name="FullName" id="FullName" required value="">
+
+    <br>
+    
+    <label for="address">Address : </label>
+    <input type="text" name="address" id="address" required value="">
+
+
+    <label for="FullName">Full Name : </label>
+    <input type="text" name="FullName" id="FullName" required value="">
+
+    <br>
+    
+    <label for="address">Address : </label>
+    <input type="text" name="address" id="address" required value="">
+
+    <label for="FullName">Full Name : </label>
+    <input type="text" name="FullName" id="FullName" required value="">
+
+    <br>
+    
+    <label for="address">Address : </label>
+    <input type="text" name="address" id="address" required value="">
+
+
+
+
+
+    
+</body>
+</html>
+
+
+
+
+<?php
+    include("footer.html")
+?>
