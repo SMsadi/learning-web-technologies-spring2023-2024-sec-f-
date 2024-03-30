a8a7ec590f88ec3e967cbf3e7eee187e654a79c5
S. M. SADI <smsadi2001@gmail.com>
2024-03-30 15:36:22 +0600

    Add files via upload

---
+++ Mid Lab Exam/homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+   <h1>Hi, It's me SADI</h1>
+   <h3>I am a student in AIUB currently studying Computer Science and Engineering(CSE)</h3>
+   <h3>Besides that I love reading novels and play football</h3>
+
+   checkout my works below here:
+   <br>
+   <a href="homepage.php">Home</a> <br>
+   <a href="photoAlbum.php">My Photo Album</a> <br>
+   <a href="portfolio.php">My Portfolio</a> <br>
+   <a href="contactpage.php">Contact with me</a> <br>
+   
+
+
+</body>
+</html>
