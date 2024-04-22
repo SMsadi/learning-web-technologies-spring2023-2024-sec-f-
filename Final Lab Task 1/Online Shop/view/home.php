7feb7fccf3dd273ede8f2278bd2cf5b46074b9ba
S. M. SADI <smsadi2001@gmail.com>
2024-04-22 13:38:11 +0600

    Add files via upload

---
+++ Final Lab Task 1/Online Shop/view/home.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('../controller/sessionCheck.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <h2>Welcome Admin</h2>
+    <a href="userlist.php">Registered Users</a><br>
+    <a href="employees.php">Employees</a><br>
+    <a href="products.php">Products list</a><br>
+    <a href="logout.php">Log out</a>
+
+</body>
+</html>
